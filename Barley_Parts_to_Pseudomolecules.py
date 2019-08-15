6663c4bdb72c88fd8a9b390c6759608c3e4944af
ChaochihL <liux1299@umn.edu>
2019-08-15 17:48:39 -0500

    add feature to take BED as input file and made compatible with python3

--- Barley_Parts_to_Pseudomolecules.py
+++ Barley_Parts_to_Pseudomolecules.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """A simple script to convert the pseudomolecule parts into full
-pseudomolecules. Remember that VCF is 1-based, which makes the math
-a lot nicer."""
+pseudomolecules."""
 
-import sys
 import argparse
 
 #   Store the lengths of the barley pseudomolecule parts in a dictionary. We only
@@ -20,23 +18,119 @@ PARTS_SIZES = {
     'chr7H_part1': 328847863
     }
 
-#   Then iterate through the VCF and print out the modified lines
-with open(sys.argv[1], 'r') as f:
-    for line in f:
-        if line.startswith('#'):
-            print line.strip()
-        else:
-            tmp = line.strip().split('\t')
-            #   modify the chromosome to not have the part
-            chrom = tmp[0].split('_')[0]
-            #   And check if we have to modify the position. If the chromosome
-            #   name has '_part2' in it, then we have to modify it.
-            if '_part2' in tmp[0]:
-                offset = part_lengths[chrom + '_part1']
-                newpos = str(int(tmp[1]) + offset)
+def parse_args():
+    """Set up an argument parser, and actually parse the args."""
+    parser = argparse.ArgumentParser(
+        description='Convert parts coordinates to pseudomolecule coordinates.',
+        add_help=True
+    )
+    # Add mutually exclusive group for the file format. This is so that only
+    # one format can be specified.
+    fmt = parser.add_mutually_exclusive_group(required=True)
+    fmt.add_argument(
+        '--vcf',
+        '-v',
+        action='store_true',
+        help='Input file is VCF'
+    )
+    fmt.add_argument(
+        '--bed',
+        '-b',
+        action='store_true',
+        help='Input file is BED'
+    )
+    parser.add_argument(
+        'intervals',
+        metavar='file',
+        help='File with intervals to convert'
+    )
+    a = parser.parse_args()
+    return a
+
+
+def set_format(args):
+    """Set the format that is being used for conversion."""
+    arg_dict = vars(args)
+    if arg_dict['vcf']:
+        return 'VCF'
+    elif arg_dict['bed']:
+        return 'BED'
+    else:
+        return None
+
+
+def vcf_conv(intervals, parts_sizes):
+    """Convert VCF. Remember that VCF is 1-based, which makes the math a lot nicer."""
+    # If lines start with '#', print them
+    with open(intervals, 'r') as f:
+        for line in f:
+            if line.startswith('#'):
+                print(line.strip())
             else:
-                newpos = tmp[1]
-            #   then print out the VCF line with the new position and chromosome
-            #   name
-            toprint = [chrom, newpos] + tmp[2:]
-            print '\t'.join(toprint)
+                tmp = line.strip().split()
+                # modify the chromosome to not have the part
+                chrom = tmp[0].split('_')[0]
+                # And check if we have to modify the position. If the chromosome
+                # name has '_part2' in it, then we have to modify it.
+                if '_part2' in tmp[0]:
+                    offset = parts_sizes[chrom + '_part1']
+                    newpos = str(int(tmp[1]) + offset)
+                else:
+                    newpos = tmp[1]
+                # then print out the VCF line with the new position and chromosome
+                # name
+                toprint = [chrom, newpos] + tmp[2:]
+                print('\t'.join(toprint))
+
+
+def bed_conv(intervals, parts_sizes):
+    """Convert BED. Remember that BED is 0-based (i.e., the first base is 0)."""
+    # If lines start with '#', print them
+    with open(intervals, 'r') as f:
+        for line in f:
+            if line.startswith('#'):
+                print(line.strip())
+                continue
+            else:
+                tmp = line.strip().split()
+                # modify the chromosome to not have th part
+                chrom = tmp[0].split('_')[0]
+                # And check if we have to modify the position. If the chromosome
+                # name has '_part2' in it, then we have to modify it.
+                if '_part2' in tmp[0]:
+                    offset = parts_sizes[chrom + '_part1']
+                    newstartpos = str(int(tmp[1]) + offset)
+                    newendpos = str(int(tmp[2]) + offset)
+                else:
+                    newstartpos = tmp[1]
+                    newendpos = tmp[2]
+                # Check if standard BED file format with 3 columns
+                if len(tmp) == 3:
+                    # Then print out BED line with chromosome name and new start and end positions
+                    toprint = [chrom, newstartpos, newendpos]
+                    print('\t'.join(toprint))
+                elif len(tmp) > 3:
+                    # Print out BED line with chromosome name and new start and end positions
+                    # along with remaining columns
+                    toprint = [chrom, newstartpos, newendpos] + tmp[3:]
+                    print('\t'.join(toprint))
+                else:
+                    print('Too few columns, please check input file is in BED format.')
+                    exit(1)
+
+
+def main():
+    """Driver function."""
+    args = parse_args()
+    f = set_format(args)
+    # Then, use the format and the supplied file to convert the coordinates
+    if f == 'VCF':
+        vcf_conv(args.intervals, PARTS_SIZES)
+    elif f == 'BED':
+        bed_conv(args.intervals, PARTS_SIZES)
+    else:
+        print('Unrecognized format, weird.')
+        exit(1)
+
+
+main() # Run the program
