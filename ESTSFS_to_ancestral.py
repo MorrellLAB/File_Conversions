43ec3ee53de5b946836112182df3897006a21ee5
Jacob Pacheco <119895935+JacobPacheco2@users.noreply.github.com>
2023-04-24 11:27:19 -0500

    Update ESTSFS_to_ancestral.py

--- ESTSFS_to_ancestral.py
+++ ESTSFS_to_ancestral.py
@@ -3,13 +3,17 @@ import gzip
 import sys
 
 
-def SecondParse(vcf_file, testfile, output_file):
+def SecondParse(vcf_file, ESTSFSFILE, output_file):
+
+    #GZIPPED VCF FILES CURRENTLY DO NOT WORK
     
-    vcf_file = gzip.open(vcf_file, 'rb') if vcf_file.endswith('.gz') else open(vcf_file, 'r')
+
+
+    vcf_file = gzip.open(vcf_file, 'rb') if vcf_file.endswith('.gz') else open(vcf_file, 'r') #parses VCF file
     vcf_reader = vcf.Reader(vcf_file)
     VCF_metadata = []
 
-    for record in vcf_reader:
+    for record in vcf_reader: # these lines parse VCF data
         chrom = record.CHROM if record.CHROM is not None else '.'
         pos = record.POS - 1 if record.POS is not None else '.'
         record_id = record.ID if record.ID is not None else '.'
@@ -20,31 +24,24 @@ def SecondParse(vcf_file, testfile, output_file):
         VCF_metadata.append([chrom, pos, record_id, ref, alt, ns, ac])
 
     
-    # vcf_reader = vcf.Reader(open('vcf_file', 'r'))
-    # info_lines = []
-    # for line in vcf_reader.metadata['INFO']:
-    #     info_lines.append(str(line))
-    # info_lines = info_lines[-5:]  
-    # for line in info_lines:
-    #     print(line)
-            
 
-    with open(testfile, "r") as fp1:
+    with open(ESTSFSFILE, "r") as fp1: #opens EST-sfs file and parses data
         with open(output_file, "w") as outfile:
-            testfile = fp1.read().splitlines()
-            Ancestrial_allele = "0"
+            testfile = fp1.read().splitlines()  
+            Ancestrial_allele = "0" #zero in case Missing data.
             i = 0  
-            for j in range(len(VCF_metadata)):
-                AC = 0
-                NS = 0
+            for j in range(len(VCF_metadata)):  
+                AC = 0 #initializes AC value
+                NS = 0 #initializes NS value
                 AC = VCF_metadata[j][6][0]
                 NS = VCF_metadata[j][5]
+                record_id = VCF_metadata[i][2]
     
                 
                 line = testfile[j]
-                Parsed_ESTSFS = line.split()
+                Parsed_ESTSFS = line.split() #puts it into a list
         
-                if Parsed_ESTSFS[0][0].isdigit() and Parsed_ESTSFS[0][0] != 0 and j> 7:
+                if Parsed_ESTSFS[0][0].isdigit() and Parsed_ESTSFS[0][0] != 0 and j> 7:  #Checks if AC and NS values are integer values, otherwises makes them 0
                     if AC == int:
                         AC = AC
                     else:
@@ -55,21 +52,24 @@ def SecondParse(vcf_file, testfile, output_file):
                     else:
                         NS = 0
 
-                    if (AC) >= NS/2:
+                    if (AC) >= NS/2:  #if AC is larger than NS/2 ancestral is AC
                         Ancestrial_allele = VCF_metadata[i][4]
-                    else: (AC) <= NS/2
+                    else: (AC) <= NS/2 # if NS/2 is larger than AC ancestral is NS
                     Ancestrial_allele = VCF_metadata[i][3]
 
          
-                    Probability_Percentage = abs((float(Parsed_ESTSFS[2]) *100))
-                    Final_parsed_string = (VCF_metadata[i][0] + " " + str(VCF_metadata[i][1] + 1) + " "  +str(VCF_metadata[i][3]) + " " +str(VCF_metadata[i][4]) + " " + 'AA=' + "'" +str(Ancestrial_allele) + "'" + " " + str(Probability_Percentage) + '\n')
+                    Probability_Percentage = abs((float(Parsed_ESTSFS[2]) *100)) # calculates the probabilty of the ancestral  
+                    Final_parsed_string = (VCF_metadata[i][0] + " " + str(VCF_metadata[i][1] + 1) + " " + str(VCF_metadata[i][6]) +str(VCF_metadata[i][3]) + " " +str(VCF_metadata[i][4]) + " " + 'AA=' + "'" +str(Ancestrial_allele) + "'" + " " + str(Probability_Percentage) + '\n') # output string
                     j += 1
-                    outfile.write(Final_parsed_string)
+                    outfile.write(Final_parsed_string) #writes final_pasrsed_string to a new file
                     i += 1
                 else:
                     j += 1
 
-vcf_file = sys.argv[1]
-testfile_path = sys.argv[2]
-output_file = sys.argv[3]
-SecondParse(vcf_file, testfile_path, output_file)
+
+#GZIPPED VCF FILES CURRENTLY DO NOT WORK
+
+vcf_file = sys.argv[1] #system input commands
+ESTSFSFILE = sys.argv[2]
+output_file = sys.argv[3] 
+SecondParse(vcf_file, ESTSFSFILE, output_file)
